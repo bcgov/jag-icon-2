16699e3f232b7f905e1da06e1dfe47a261069a22
jianming tu <tu_jm@yahoo.com>
2022-04-14 12:05:50 -0700

    split application to SOAP 1.1 and 1.2

---
+++ icon-two-common-application-1-2/src/main/java/icon/IconTwoCommonApplication.java
@@ -0,0 +1,11 @@
+package icon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IconTwoCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IconTwoCommonApplication.class, args);
+    }
+}
