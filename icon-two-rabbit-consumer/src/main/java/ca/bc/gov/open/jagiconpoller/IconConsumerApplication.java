ea58358bba051bb3d4dc52963d2a568bc1ece8a1
ekwan <224302@nttdata.com>
2022-05-09 15:26:53 -0700

    add queue config

---
+++ icon-two-rabbit-consumer/src/main/java/ca/bc/gov/open/jagiconpoller/IconConsumerApplication.java
@@ -0,0 +1,12 @@
+package ca.bc.gov.open.jagiconpoller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IconConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IconConsumerApplication.class, args);
+    }
+}
---
+++ icon-two-rabbit-consumer/src/main/java/ca/bc/gov/open/jagiconpoller/services/PacConsumerService.java
@@ -0,0 +1,24 @@
+package ca.bc.gov.open.jagiconpoller.services;
+
+import ca.bc.gov.open.icon.models.PACModel;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PacConsumerService {
+
+    @RabbitListener(queues = "${icon.pac-queue}")
+    public void receivedMessage(@Payload Message<PACModel> message) throws JsonProcessingException {
+        System.out.println(new ObjectMapper().writeValueAsString(message.getPayload()));
+    }
+
+    @RabbitListener(queues = "${icon.ping-queue}")
+    public void receivedTestMessage(@Payload Message<PACModel> message)
+            throws JsonProcessingException {
+        System.out.println(new ObjectMapper().writeValueAsString(message.getPayload()));
+    }
+}
