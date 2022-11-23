da137574250364f2d86fc9209a1bc01dfb13329b
jianming tu <tu_jm@yahoo.com>
2022-11-22 18:09:14 -0800

    rename

--- icon-two-common-models/src/main/java/ca/bc/gov/open/icon/models/serializers/BooleanSoapConverter.java
+++ icon-two-common-models/src/main/java/ca/bc/gov/open/icon/models/serializers/BooleanSoapConverter.java
@@ -3,8 +3,8 @@ package ca.bc.gov.open.icon.models.serializers;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public final class BooleanConverter {
-    private BooleanConverter() {}
+public final class BooleanSoapConverter {
+    private BooleanSoapConverter() {}
 
     public static String print(boolean b) {
         return b ? "1" : "0";
