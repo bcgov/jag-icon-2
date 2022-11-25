6fbf9bd931da4298da3554839f05411c26bd32c6
jianming tu <tu_jm@yahoo.com>
2022-11-25 15:34:43 -0800

    concise code

--- icon-two-common-models/src/main/java/ca/bc/gov/open/icon/models/serializers/BooleanSoapConverterAdapter.java
+++ icon-two-common-models/src/main/java/ca/bc/gov/open/icon/models/serializers/BooleanSoapConverterAdapter.java
@@ -5,10 +5,10 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 public class BooleanSoapConverterAdapter extends XmlAdapter<String, Boolean> {
 
     public Boolean unmarshal(String value) {
-        return (ca.bc.gov.open.icon.models.serializers.BooleanSoapConverter.parse(value));
+        return BooleanSoapConverter.parse(value);
     }
 
     public String marshal(Boolean value) {
-        return (ca.bc.gov.open.icon.models.serializers.BooleanSoapConverter.print(value));
+        return BooleanSoapConverter.print(value);
     }
 }
--- icon-two-common-models/src/main/java/ca/bc/gov/open/icon/models/serializers/InstantSoapConverterAdapter.java
+++ icon-two-common-models/src/main/java/ca/bc/gov/open/icon/models/serializers/InstantSoapConverterAdapter.java
@@ -5,10 +5,10 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class InstantSoapConverterAdapter extends XmlAdapter<String, Instant> {
     public Instant unmarshal(String value) {
-        return (ca.bc.gov.open.icon.models.serializers.InstantSoapConverter.parse(value));
+        return InstantSoapConverter.parse(value);
     }
 
     public String marshal(Instant value) {
-        return (ca.bc.gov.open.icon.models.serializers.InstantSoapConverter.print(value));
+        return InstantSoapConverter.print(value);
     }
 }
