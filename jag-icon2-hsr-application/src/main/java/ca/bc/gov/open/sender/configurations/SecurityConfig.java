1291bedc3b82e343c9a189e26493854490c6b3c8
jianming tu <tu_jm@yahoo.com>
2023-02-09 12:49:12 -0800

    add ping and health

--- jag-icon2-hsr-application/src/main/java/ca/bc/gov/open/sender/configurations/SecurityConfig.java
+++ jag-icon2-hsr-application/src/main/java/ca/bc/gov/open/sender/configurations/SecurityConfig.java
@@ -1,10 +1,12 @@
-package ca.bc.gov.open.icon.configuration;
+package ca.bc.gov.open.sender.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,23 +17,40 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    @Value("${security.basic-auth.username}")
+    private String userName;
+
+    @Value("${security.basic-auth.password}")
+    private String password;
+
     @Autowired private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/secure")
-                .authenticated()
+        http.authorizeRequests()
+                .antMatchers("/error")
+                .permitAll()
                 .anyRequest()
-                .permitAll();
+                .authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.builder().roles("Admin").build();
+        UserDetails user =
+                User.builder()
+                        .username(userName)
+                        .password(passwordEncoder().encode(password))
+                        .roles("Admin")
+                        .build();
         return new InMemoryUserDetailsManager(user);
     }
 
