fc39454139aa2b85db46b80b1526c16f13bc43f8
TayGov <taylor.clausen@gov.bc.ca>
2026-09-10 10:19:43 -0700

    Update starting application

--- jag-icon2-auth-application/src/main/java/ca/bc/gov/open/icon/IconTwoAuthApplication.java
+++ jag-icon2-auth-application/src/main/java/ca/bc/gov/open/icon/IconTwoAuthApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IconTwoCommonApplication {
+public class IconTwoAuthApplication {
     public static void main(String[] args) {
-        SpringApplication.run(IconTwoCommonApplication.class, args);
+        SpringApplication.run(IconTwoAuthApplication.class, args);
     }
 }
